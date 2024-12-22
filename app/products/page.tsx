d5e8207dfb4934a70351fd0d18d593e933f35d91
Leonardo Brizolla <leonardobrizollars@gmail.com>
2024-12-22 17:17:03 -0300

    Add Products and Sales pages with button components

---
+++ app/products/page.tsx
@@ -0,0 +1,5 @@
+import { Button } from "@/app/_components/ui/button";
+
+export default function Products() {
+  return <Button>Produtos</Button>;
+}
---
+++ app/sales/page.tsx
@@ -0,0 +1,5 @@
+import { Button } from "../_components/ui/button";
+
+export default function Sales() {
+  return <Button>Vendas</Button>;
+}
