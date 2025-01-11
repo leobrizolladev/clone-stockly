e8427160242b261124a589e8edd16623013bf4e9
Leonardo Brizolla <leonardobrizollars@gmail.com>
2025-01-10 23:45:03 -0300

    feat: Implement Prisma client setup and enhance Products page layout with new UI elements

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,25 @@
-import { Button } from "@/app/_components/ui/button";
+import { PlusCircleIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
+// import { db } from "../_lib/prisma";
 
-export default function Products() {
-  return <Button>Produtos</Button>;
+export default async function Products() {
+  // const products = await db.product.findMany();
+
+  return (
+    <div className="w-full space-y-8 p-8">
+      <div className="flex w-full items-center justify-between">
+        <div className="space-y-1">
+          <span className="text-xs font-semibold text-slate-500">
+            Gestão de Produtos
+          </span>
+          <h2 className="text-xl font-bold">Produtos</h2>
+        </div>
+
+        <Button className="gap-2">
+          <PlusCircleIcon size={20} />
+          Novo Produto
+        </Button>
+      </div>
+    </div>
+  );
 }
